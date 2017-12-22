fa9b3682639035bc360addde26e11fca8efcb398
Bret Walker <bw@bretw.com>
2017-12-22 15:22:49 -0500

    Testing

--- sql/V1__Init.sql
+++ sql/V1__Init.sql
@@ -3,7 +3,3 @@ CREATE TABLE car (
     license_plate VARCHAR NOT NULL,
     color VARCHAR NOT NULL
 );
-
-ALTER TABLE owner ADD driver_license_id VARCHAR;
-
-INSERT INTO brand (name) VALUES ('DeLorean');
