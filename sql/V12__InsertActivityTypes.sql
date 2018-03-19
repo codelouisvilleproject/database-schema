d0405b14dcb3ff2b1b69f4ebb015cb0eaa2936bf
Nesto <bumbolio@gmail.com>
2018-03-19 19:32:08 -0400

    Converted double quotes to single.

--- sql/V12__InsertActivityTypes.sql
+++ sql/V12__InsertActivityTypes.sql
@@ -1,4 +1,4 @@
-INSERT into activitytypes (name, description) values ("Run", "Running");
-INSERT into activitytypes (name, description) values ("Walk", "Walking");
-INSERT into activitytypes (name, description) values ("Swim", "Swimming");
-INSERT into activitytypes (name, description) values ("Bike", "Biking");
+INSERT into activitytypes (name, description) values ('Run', 'Running');
+INSERT into activitytypes (name, description) values ('Walk', 'Walking');
+INSERT into activitytypes (name, description) values ('Swim', 'Swimming');
+INSERT into activitytypes (name, description) values ('Bike', 'Biking');
