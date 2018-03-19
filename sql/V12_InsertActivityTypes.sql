3ce1ee5c7dce88e49d3f55101d9b9a28f8193170
Nesto <bumbolio@gmail.com>
2018-03-19 19:14:48 -0400

    Added activity types.

---
+++ sql/V12_InsertActivityTypes.sql
@@ -0,0 +1,4 @@
+INSERT into activitytypes values("0", "Walk", "Walking");
+INSERT into activitytypes values("1", "Run", "Running");
+INSERT into activitytypes values("2", "Swim", "Swimming");
+INSERT into activitytypes values("3", "Bike", "Biking");
